bec4c168fc9f00adec4645825bd3b719259d9f3a
李元琦 <yuan.chi@program.com.tw>
2020-03-25 16:44:29 +0800

    環境調整

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
 
--- client/src/views/TestRoom.js
+++ client/src/views/TestRoom.js
@@ -5,15 +5,11 @@ const TestRoom = () => {
   const [userName, setUserName] = useState("");
   const [inputMessage, setInputMessage] = useState("");
   const [incomingMessages, setIncomingMessages] = useState([{ name: "", message: "" }]);
-  const socket = socketIoClient('http://localhost:3000');
-  // socket.on('new message', (data) => {
-  //   addChatMessage(data);
-  // });
+  // const [socket, setSocket] = useState(null);
 
-  // // socket.on('user joined', (data) => {
-  //   console.log(data.username + ' joined');
-  // });
+  const socket = socketIoClient('http://localhost:3000');
   useEffect(() => {
+    console.log('ss');
     if (socket) {
       //連線成功在 console 中打印訊息
       console.log('success connect!')
@@ -22,29 +18,37 @@ const TestRoom = () => {
     }
   }, [socket])
 
+  // socket.on('new message', (data) => {
+  //   addChatMessage(data);
+  // });
+
+  // // socket.on('user joined', (data) => {
+  //   console.log(data.username + ' joined');
+  // });
+
 
   const initSocketIoClient = () => {
     console.log("initSocketIoClient")
   }
   const login = (userName) => {
-    console.log(userName)
-    // // Tell the server username
-    // socket.emit('add user', userName);
+    console.log(userName);
+    setUserName(userName);
   }
 
-  // const sendMessage = (inputMessage) => {
-  //   var message = inputMessage;
-  //   setInputMessage("");
-  //   if (socket) {
-  //     addChatMessage({
-  //       name: userName,
-  //       message: message
-  //     });
-  //   }
-  //   socket.emit('new message', message);
-  // }
+  const sendMessage = (inputMessage) => {
+    var message = inputMessage;
+    setInputMessage("");
+    if (socket) {
+      addChatMessage({
+        name: userName,
+        message: message
+      });
+    }
+    // socket.emit('new message', message);
+  }
 
   const addChatMessage = (data, options) => {
+    console.log("new message")
     setIncomingMessages([...incomingMessages, { name: data.name, message: data.message }]);
   }
 
@@ -56,31 +60,34 @@ const TestRoom = () => {
         <input
           value={userName}
           placeholder="ex: ㄩㄑ"
-          onChange={(event) => setUserName(event.target.value)}></input>
-        <button
-          className="ml-2"
-          disabled={(userName.length > 0) ? false : true}
-          onClick={() => login(userName)}>OK</button>
+          onChange={(event) => login(event.target.value)}></input>
+        {/* <button
+            type="submit"
+            className="ml-2"
+            disabled={(userName.length > 0) ? false : true}
+            onClick={() => login(userName)}>OK</button> */}
+
       </div>
 
       <div className="MessageArea">
         <ul className="Messages">
           <li className="Message">{userName} 歡迎進入聊天室 :D</li>
           {incomingMessages.map((item) => (
-            <li key={item.mseeage} className="Message">{item.name} {item.message}</li>
+            <li key={item.message} className="Message">{item.name} {item.message}</li>
           ))}
         </ul>
       </div>
-      <div className="InputeArea">
+      <form className="InputeArea" onSubmit={sendMessage}>
         <input
           value={inputMessage}
           placeholder="say something :)"
           onChange={(event) => setInputMessage(event.target.value)}></input>
-        {/* <button
+        <button
+          type="submit"
           className="ml-2"
           disabled={(inputMessage.length > 0) ? false : true}
-          onClick={() => sendMessage(inputMessage)} >send</button> */}
-      </div>
+          onClick={() => sendMessage(inputMessage)} >send</button>
+      </form>
     </div>
   );
 }
